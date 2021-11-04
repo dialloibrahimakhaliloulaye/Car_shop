7d50e46659c76bfe430919cc739e06fec02ade64
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2021-11-04 19:19:19 +0000

    creating auth screen design

--- lib/authenticationScreen.dart
+++ lib/authenticationScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:icar/login.dart';
+import 'package:icar/register.dart';
 
 class AuthenticationScreen extends StatefulWidget {
 
@@ -9,6 +11,63 @@ class AuthenticationScreen extends StatefulWidget {
 class _AuthenticationScreenState extends State<AuthenticationScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            flexibleSpace: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.amber,
+                    Colors.green,
+                  ],
+                  begin: const FractionalOffset(0.0, 0.0),
+                  end: const FractionalOffset(1.0, 0.0),
+                  stops: [0.0, 1.0],
+                  tileMode: TileMode.clamp
+                )
+              ),
+            ),
+            title: Text("Car Shop",
+              style: TextStyle(fontSize: 60.0, color: Colors.white, fontFamily: "Lobster"),
+            ),
+            centerTitle: true,
+            bottom: TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.lock, color: Colors.white,),
+                  text: "Se connecter",
+                ),
+                Tab(
+                  icon: Icon(Icons.person, color: Colors.white,),
+                  text: "S'inscrire",
+                )
+              ],
+              indicatorColor: Colors.white38,
+              indicatorWeight: 5.0,
+
+            ),
+          ),
+          body: Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                      end: Alignment.bottomLeft,
+                      colors: [
+                        Colors.green,
+                        Colors.amber,
+                      ],
+                  )
+              ),
+            child: TabBarView(
+              children: [
+                Login(),
+                Register(),
+              ],
+            ),
+          ),
+        )
+    );
   }
 }
