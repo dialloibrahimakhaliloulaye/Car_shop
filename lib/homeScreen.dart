e923eb5757b510f3cae2c07ae396e4a111b895f1
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2021-11-06 23:50:30 +0000

    add home screen UI

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -10,6 +10,71 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.refresh, color: Colors.white,),
+          onPressed: (){
+
+          },
+        ),
+        actions: <Widget>[
+          TextButton(
+              onPressed: (){
+
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Icon(Icons.person, color: Colors.white,),
+              )
+          ),
+          TextButton(
+              onPressed: (){
+
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Icon(Icons.search, color: Colors.white,),
+              )
+          ),
+          TextButton(
+              onPressed: (){
+
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Icon(Icons.login_outlined, color: Colors.white,),
+              )
+          )
+        ],
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [
+                    Colors.amber,
+                    Colors.green
+                  ],
+                  begin: const FractionalOffset(0.0, 0.0),
+                  end: const FractionalOffset(1.0, 0.0),
+                  stops: [0.0, 1.0],
+                  tileMode: TileMode.clamp
+              )
+          ),
+        ),
+        title: Text("Accueil"),
+      ),
+      body: Center(
+        child: Container(
+
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: "Ajouter l'annonce",
+        child: Icon(Icons.add),
+        onPressed: (){
+
+        },
+      ),
+    );
   }
 }
